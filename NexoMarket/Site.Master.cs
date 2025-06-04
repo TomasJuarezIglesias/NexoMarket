83981c1e9c4e46cf28aa13e3aa50c7566a123ce6
Tomas Juarez <tjuarez.tm@hotmail.com>
2025-06-04 20:25:52 -0300

    Agregado evento logout

--- NexoMarket/Site.Master.cs
+++ NexoMarket/Site.Master.cs
@@ -3,6 +3,7 @@ using NexoMarket.Business;
 using NexoMarket.Entity;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Security;
 using System.Web.UI;
@@ -12,6 +13,14 @@ namespace NexoMarket
     public partial class SiteMaster : MasterPage
     {
         public List<MenuEntity> MenusList;
+        private readonly BusinessBitacora _businessBitacora;
+
+        public SiteMaster()
+        {
+            MenusList = new List<MenuEntity>();
+            _businessBitacora = new BusinessBitacora();
+        }
+
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -25,10 +34,7 @@ namespace NexoMarket
             // Armado de sideBar
             if (Request.IsAuthenticated)
             {
-                FormsIdentity identity = (FormsIdentity)HttpContext.Current.User.Identity;
-                FormsAuthenticationTicket ticket = identity.Ticket;
-
-                var userData = JsonConvert.DeserializeObject<UserAuthEntity>(ticket.UserData);
+                var userData = GetUserAuthenticated();
                 MenusList = userData.AllowedMenues ?? new List<MenuEntity>();
 
                 // Validación Ruta
@@ -41,11 +47,23 @@ namespace NexoMarket
 
         protected void btnLogout_Click(object sender, EventArgs e)
         {
+            var user = GetUserAuthenticated();
+
+            // Ejecuión en segundo plano
+            Task.Run(() => _businessBitacora.GuardarEventoBitacora("Logout", user.Id));
+
             FormsAuthentication.SignOut();
             Session.Abandon();
             Response.Redirect("Login.aspx");
         }
 
+        private UserAuthEntity GetUserAuthenticated()
+        {
+            FormsIdentity identity = (FormsIdentity)HttpContext.Current.User.Identity;
+            FormsAuthenticationTicket ticket = identity.Ticket;
+
+            return JsonConvert.DeserializeObject<UserAuthEntity>(ticket.UserData);
+        }
 
     }
 }
